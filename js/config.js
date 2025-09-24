c951ba6ff07b7f10944cfa09c21f50e057093d7f
DevloperScaner <anjaniselalu76@gmail.com>
2025-09-25 02:07:57 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // js/config.js — GANTI dengan config Firebase kamu
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Xlu02Winh4wp2XDzw5592yE9_J-qiuM",
+  authDomain: "investasi-hewan.firebaseapp.com",
+  projectId: "investasi-hewan",
+  storageBucket: "investasi-hewan.firebasestorage.app",
+  messagingSenderId: "180728484800",
+  appId: "1:180728484800:web:f2fcd8e390b636dfb6ee25"
 };
 
 // OPTIONAL: Set to true jika kamu pakai custom claims admin via Cloud Functions
