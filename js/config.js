52b9344b200396688818a5dd647809c7e02bc0cb
DevloperScaner <anjaniselalu76@gmail.com>
2025-09-25 17:30:31 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-
 export const firebaseConfig = {
   apiKey: "AIzaSyB7Xlu02Winh4wp2XDzw5592yE9_J-qiuM",
   authDomain: "investasi-hewan.firebaseapp.com",
@@ -7,7 +6,14 @@ export const firebaseConfig = {
   messagingSenderId: "180728484800",
   appId: "1:180728484800:web:f2fcd8e390b636dfb6ee25"
 };
+
 export const cloudinary = { cloudName: "dn1eh1jfi", uploadPreset: "Animal-cash-main" };
+
+export const adminAllowlist = {
+  emails: ["airdropshunterid@gmail.com"],
+  uids: []
+};
+
 export const defaults = {
   loading: { durationMs: 10000, mode: "fixed", title: "ANIMALS CASH", theme: "rainbow" },
   theme: { default: "system", accent: "#59d1ff", animatedBrand: true, seasonalActive: false, seasonalName: "" },
